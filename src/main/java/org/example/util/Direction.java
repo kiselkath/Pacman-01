8fc2d502a37fdf3f358d9dd489ca0b52de54a39b
Kate kate <kiselkath@gmail.com>
2026-01-04 19:12:53 +0100

    Updated util

--- src/main/java/org/example/util/Direction.java
+++ src/main/java/org/example/util/Direction.java
@@ -1,4 +1,40 @@
 package org.example.util;
 
-public class Direction {
+/**
+ * Domain model: Direction enum
+ * Represents movement direction in the game world.
+ * Domain layer - no Swing dependencies.
+ */
+public enum Direction {
+    UP('U'),
+    DOWN('D'),
+    LEFT('L'),
+    RIGHT('R');
+
+    private final char charValue;
+
+    Direction(char charValue) {
+        this.charValue = charValue;
+    }
+
+    public char getCharValue() {
+        return charValue;
+    }
+
+    /**
+     * Convert character to Direction enum
+     */
+    public static Direction fromChar(char c) {
+        if (c == 'U') {
+            return UP;
+        } else if (c == 'D') {
+            return DOWN;
+        } else if (c == 'L') {
+            return LEFT;
+        } else if (c == 'R') {
+            return RIGHT;
+        } else {
+            throw new IllegalArgumentException("Unknown direction: " + c);
+        }
+    }
 }
--- src/main/java/org/example/util/Position.java
+++ src/main/java/org/example/util/Position.java
@@ -1,4 +1,54 @@
 package org.example.util;
 
+/**
+ * Domain model: Position
+ * Represents a position in the game world with x, y coordinates.
+ * Domain layer - no Swing dependencies.
+ */
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void add(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public Position copy() {
+        return new Position(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
 }
