7837949cdec5038bf925df8ee480cc21e495c96a
Kate kate <kiselkath@gmail.com>
2026-01-04 19:05:19 +0100

    Updated Creature logic

--- src/main/java/org/example/entity/CreatureType.java
+++ src/main/java/org/example/entity/CreatureType.java
@@ -1,4 +1,14 @@
 package org.example.entity;
 
+/**
+ * Domain model: CreatureType enum
+ * Represents different types of creatures in the game.
+ * Domain layer - no Swing dependencies.
+ */
 public enum CreatureType {
+    PACMAN,      // Player character
+    RED_GHOST,   // Red ghost
+    BLUE_GHOST,  // Blue ghost
+    PINK_GHOST,  // Pink ghost
+    ORANGE_GHOST // Orange ghost
 }
